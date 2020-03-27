4bcfa56f6ccee3e000b4f53b436cabb1d1f3180f
Saif Ansari <sga786920@gmail.com>
2020-03-27 16:01:54 +0530

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import * as OT from '@opentok/client';
   providers: [ OpentokService ]
 })
 export class AppComponent implements OnInit {
-  title = 'Angular Video Call Demo';
+  title = 'Angular Video Chat demo';
   session: OT.Session;
   streams: Array<OT.Stream> = [];
   changeDetectorRef: ChangeDetectorRef;
